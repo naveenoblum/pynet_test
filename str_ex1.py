155d683e06dfbed46a839c70c28f62c77caf3a51
Naveen Oblumpally <naveeno@hotmail.com>
2017-10-16 12:47:23 -0700

    String exercises

---
+++ str_ex1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+ip_addr_1="1.1.1.1"
+ip_addr_2="2.2.2.2"
+ip_addr_3="3.3.3.3"
+
+print("{:>30}{:>30}{:>30}".format(ip_addr_1,ip_addr_2,ip_addr_3))
+
+ip_addr_4=raw_input("Enter the 4th IP addr:")
+print (ip_addr_4)
+
---
+++ str_ex2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+ip_addr = raw_input("Input your IP address:")
+ip_list=ip_addr.split(".")
+print("{:<12}{:<12}{:<12}{:<12}".format(ip_list[0],ip_list[1],ip_list[2],ip_list[3]))

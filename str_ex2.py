bf49e969a6fe918e63cb19f0c4bb20971f080de6
Naveen Oblumpally <naveeno@hotmail.com>
2017-10-16 12:52:34 -0700

    modified

--- str_ex2.py
+++ str_ex2.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 ip_addr = raw_input("Input your IP address:")
+
+#ip_list=ip_addr.split(".")
+#print("{:<12}{:<12}{:<12}{:<12}".format(ip_list[0],ip_list[1],ip_list[2],ip_list[3]))
+
 ip_list=ip_addr.split(".")
-print("{:<12}{:<12}{:<12}{:<12}".format(ip_list[0],ip_list[1],ip_list[2],ip_list[3]))
+print("{:<12}{:<12}{:<12}{:<12}".format(*ip_list))
